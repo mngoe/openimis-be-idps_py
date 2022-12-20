b23738aa62dd00df0193e45359d9eca0ec864182
FrancineMADOH <francinemadoh@gmail.com>
2022-12-20 17:59:53 +0100

    insertion des data + contrainte sur les dates

--- idps/models.py
+++ idps/models.py
@@ -14,9 +14,9 @@ class PerformanceCriteria(models.Model):
     id = models.SmallIntegerField(db_column='ID', primary_key=True)
     date_from =  core.fields.DateField(db_column='StartDate', blank=True, null=True)
     date_to = core.fields.DateField(db_column='EndDate', blank=True, null= True)
-    health_facility =  models.ForeignKey(location_models.HealthFacility, models.DO_NOTHING, db_column='HFID', blank=True, null=True)
+    health_facility = models.IntegerField(db_column='HFID', blank=True, null=True)
+    #health_facility =  models.ForeignKey(location_models.HealthFacility, models.DO_NOTHING, db_column='HFID', blank=True, null=True)
     promptness =  models.IntegerField(db_column='PromptnessInvoiceSubmission')
-    degree_of_rejection =  models.DecimalField(db_column='DegreeofInvoiceRejection', decimal_places=2,max_digits=18,blank=True, null=True)
     medecine_availability = models.IntegerField(db_column='MedecineAvailability',blank=True, null= True)
     qualified_personnel =  models.IntegerField(db_column='QualifiedPersonel',blank=True, null= True)
     garbagecan_availability = models.IntegerField(db_column='GarbagecanAvailable',blank=True, null= True)
@@ -24,13 +24,13 @@ class PerformanceCriteria(models.Model):
     waste_separation = models.IntegerField(db_column='WasteSeparationSystem',blank=True, null= True)
     functionals_toilets =  models.IntegerField(db_column='FunctionalToilets',blank=True, null= True)
     sterilization_tools = models.IntegerField(db_column='SterilizationTools',blank=True, null= True)
-
+    is_validated = models.BooleanField(db_column='isValidated', blank=True, null=True, default=False)
+    record_date = models.DateTimeField(db_column='RecordDate',blank=True, null=True)
+    #audit_user_id = models.IntegerField(db_column='AuditUserId',blank=True, null= True)
     class Meta:
         managed=True
         db_table = 'tblPerformanceCriteria'
 
-
-# prevent multiples insertions on the same period for the same hf in this period
 
 
 
